b7ba3f02283941751f1e4cb0c961518ebd27a969
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-03-22 23:13:16 +0100

    Publish build scans (#30)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,18 @@ pluginManagement {
 
 plugins { 
     id("myRepos")
+    id("com.gradle.enterprise") version "3.12.4"
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        if (System.getenv("CI") != null) {
+            publishAlways()
+            tag("CI")
+        }
+    }
 }
 
 rootProject.name = "serviceloader-gradle-plugin"
