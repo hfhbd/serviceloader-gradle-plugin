730ffcb157407b516dc76616bd8f731402b707fd
hfhbd <hfhbd@users.noreply.github.com>
2023-03-25 21:50:17 +0100

    Fix mpp

--- ksp-sample/settings.gradle.kts
+++ ksp-sample/settings.gradle.kts
@@ -8,7 +8,6 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         mavenCentral()
         gradlePluginPortal()
