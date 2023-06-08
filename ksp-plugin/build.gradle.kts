ace1b4844fa87af2ba3c9e6c6cdc7e2f039176e8
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2023-06-08 11:53:03 +0200

    Use Gradle version catalog (#59)
    Co-authored-by: hfhbd <hfhbd@users.noreply.github.com>

--- ksp-plugin/build.gradle.kts
+++ ksp-plugin/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlinSetup
-    `maven-publish`
+    id("kotlinSetup")
+    id("maven-publish")
 }
 
 publishing.publications.register<MavenPublication>("mavenJava") {
@@ -9,5 +9,5 @@ publishing.publications.register<MavenPublication>("mavenJava") {
 
 dependencies {
     implementation(projects.kspAnnotation)
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.8.21-1.0.11")
+    implementation(libs.ksp.api)
 }
