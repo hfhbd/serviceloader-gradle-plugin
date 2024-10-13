403a31ff6ea9867df4a6eeb729f421dd073e6ac9
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-10-13 19:28:52 +0200

    Fix nested supertype

--- kotlin-plugin/src/test/kotlin/app/softwork/validation/plugin/kotlin/kct.kt
+++ kotlin-plugin/src/test/kotlin/app/softwork/validation/plugin/kotlin/kct.kt
@@ -1,17 +1,11 @@
 package app.softwork.validation.plugin.kotlin
 
 import app.softwork.serviceloader.plugin.kotlin.ServiceLoaderCompilerPluginRegistrar.Companion.registerServiceLoader
-import app.softwork.serviceloader.plugin.kotlin.ServiceLoaderInitExtensionRegistrar
-import com.tschuchort.compiletesting.DiagnosticSeverity
 import com.tschuchort.compiletesting.KotlinCompilation
 import com.tschuchort.compiletesting.KotlinCompilation.ExitCode
 import com.tschuchort.compiletesting.SourceFile
-import org.jetbrains.kotlin.backend.common.extensions.*
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import kotlin.contracts.InvocationKind
-import kotlin.contracts.contract
-import kotlin.test.assertEquals
 
 fun jvmCompile(vararg files: SourceFile, writeFile: (String, String) -> Unit) {
     val result = KotlinCompilation()
