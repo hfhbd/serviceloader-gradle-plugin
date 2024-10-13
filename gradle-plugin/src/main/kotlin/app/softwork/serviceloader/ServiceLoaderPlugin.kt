f460d1de17d726d27d612596232532f9606ecd54
Philip Wedemann <22521688+hfhbd@users.noreply.github.com>
2024-10-13 20:34:19 +0200

    Fix resources directory task dependency (#126)

--- gradle-plugin/src/main/kotlin/app/softwork/serviceloader/ServiceLoaderPlugin.kt
+++ gradle-plugin/src/main/kotlin/app/softwork/serviceloader/ServiceLoaderPlugin.kt
@@ -5,13 +5,7 @@ import org.gradle.api.provider.Provider
 import org.gradle.kotlin.dsl.listProperty
 import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
-import org.jetbrains.kotlin.gradle.plugin.FilesSubpluginOption
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilation
-import org.jetbrains.kotlin.gradle.plugin.KotlinCompilerPluginSupportPlugin
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
-import org.jetbrains.kotlin.gradle.plugin.SubpluginArtifact
-import org.jetbrains.kotlin.gradle.plugin.SubpluginOption
-import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinJvmCompilation
+import org.jetbrains.kotlin.gradle.plugin.*
 
 public class ServiceLoaderPlugin : KotlinCompilerPluginSupportPlugin {
     override fun apply(target: Project) {
@@ -49,29 +43,25 @@ public class ServiceLoaderPlugin : KotlinCompilerPluginSupportPlugin {
     )
 
     override fun applyToCompilation(kotlinCompilation: KotlinCompilation<*>): Provider<List<SubpluginOption>> {
-        val project = kotlinCompilation.target.project
-        val outputDir =
-            project.layout.buildDirectory.dir("generated/serviceloader/${kotlinCompilation.defaultSourceSet.name}/resources")
+        val project = kotlinCompilation.project
+        val kotlinSourceSet = kotlinCompilation.defaultSourceSet
+
+        val outputDir = project.layout.buildDirectory.dir(
+            "generated/serviceloader/${kotlinSourceSet.name}/resources"
+        )
+
         val compileTaskProvider = kotlinCompilation.compileTaskProvider
         compileTaskProvider.configure {
             outputs.dir(outputDir)
         }
-        val kotlinSourceSet = kotlinCompilation.defaultSourceSet
-        kotlinSourceSet.resources.srcDir(outputDir)
-        val processResourcesTaskName = if (kotlinSourceSet.name == "main") {
-            "processResources"
-        } else {
-            if (project.pluginManager.hasPlugin("org.jetbrains.kotlin.multiplatform")) {
-                kotlinSourceSet.name.removeSuffix("Main") + "ProcessResources"
-            } else {
-                "process" + kotlinSourceSet.name.replaceFirstChar { it.uppercaseChar() } + "Resources"
-            }
-        }
-        kotlinCompilation.project.tasks.named(processResourcesTaskName) {
-            dependsOn(compileTaskProvider)
+
+        val outputDirWithTask = project.files(outputDir) {
+            builtBy(compileTaskProvider)
         }
 
-        val options = kotlinCompilation.project.objects.listProperty<SubpluginOption>()
+        kotlinSourceSet.resources.srcDir(outputDirWithTask)
+
+        val options = project.objects.listProperty<SubpluginOption>()
         options.add(
             FilesSubpluginOption(
                 key = "outputDir",
