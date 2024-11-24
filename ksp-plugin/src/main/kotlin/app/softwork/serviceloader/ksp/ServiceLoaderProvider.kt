acac3dc83c98737aa51ed95cd1a9e67a590eecda
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-11-24 14:30:18 +0100

    Fix binary name in KSP and support multiple rounds

--- ksp-plugin/src/main/kotlin/app/softwork/serviceloader/ksp/ServiceLoaderProvider.kt
+++ ksp-plugin/src/main/kotlin/app/softwork/serviceloader/ksp/ServiceLoaderProvider.kt
@@ -1,8 +1,10 @@
 package app.softwork.serviceloader.ksp
 
+import com.google.devtools.ksp.KspExperimental
 import com.google.devtools.ksp.processing.*
 
+@KspExperimental
 public class ServiceLoaderProvider : SymbolProcessorProvider {
     override fun create(environment: SymbolProcessorEnvironment): ServiceLoaderPlugin =
-        ServiceLoaderPlugin(environment.codeGenerator)
+        ServiceLoaderPlugin(environment.codeGenerator, environment.logger)
 }
